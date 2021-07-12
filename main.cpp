bbf30217e33d25381dd4d1aa45fd9b15a5794019
Luca Capecchi <lucapecchi13@gmail.com>
2021-07-12 16:09:36 +0200

    questions

--- main.cpp
+++ main.cpp
@@ -5,16 +5,12 @@
 
 using namespace std;
 
+
+// shoudl I make this abstract and
 class Question {
-protected:
-    string question;
-    string correct_answer;
 
 public:
-    Question(string que, string corr_ans) {
-        question = que;
-        correct_answer = corr_ans;
-    };
+    Question(string que, string corr_ans):question(que),correct_answer(corr_ans) {};
    
 
     virtual bool ask_question() {
@@ -28,14 +24,16 @@ public:
 
     }
 
+protected:
+    string question;
+    string correct_answer;
+
 };
 
 class MultipleChoiseQuestion : public Question {
 public:
-    MultipleChoiseQuestion(string que, string corr_ans, vector<string> ans):Question(que, corr_ans){
-        answers = ans;
-    };
-
+    // isn't it hard to read?
+    MultipleChoiseQuestion(string que, string corr_ans, vector<string> ans):Question(que, corr_ans),answers(ans){};
 
     bool ask_question() override {
         cout << question << endl;
@@ -68,18 +66,19 @@ private:
 int main() {
     
     int point = 0;
-    vector<Question*> list;
+    vector<Question> list;
     vector<string> ans;
-    list.push_back(new Question("What is always coming but never arrives? ","tomorrow"));
+    // when should I use new?
+    list.push_back(Question("What is always coming but never arrives? ","tomorrow"));
     ans = { "usa", "australia", "south africa", "new zeland" };
-    list.push_back(new MultipleChoiseQuestion("Which country do cities of Perth, Adelade & Brisbane belong to?", "australia", ans));
-    cout << "There are 10 question, some with multiple choise where you have to select the correct one" << endl;
+    list.push_back(MultipleChoiseQuestion("Which country do cities of Perth, Adelade & Brisbane belong to?", "australia", ans));
+    cout << "There are "<< list.size() << " question, some with multiple choise where you have to select the correct one" << endl;
     cout << "others you should write the answer, use lowercase and there should be no problem" << endl;
     cout << "Good Luck" << endl;
-    for each (Question * var in list) {
+    for each (Question var in list) {
         cout << "------------------------------------------------------" << endl;
 
-        if (var->ask_question())
+        if (var.ask_question())
             point++;
     }
     cout << "Your score is: " << point << endl;
